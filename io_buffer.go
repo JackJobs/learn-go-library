6f529c8fef10ab3957c59c9092bda7e8520867a1
zshanjun <937587592jack@gmail.com>
2017-09-09 16:31:08 +0800

    Create io_buffer.go

---
+++ io_buffer.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"bufio"
+)
+
+func main() {
+	strReader := strings.NewReader("hello world")
+	bufReader := bufio.NewReader(strReader)
+	data, _ := bufReader.Peek(5)
+	fmt.Println(data, bufReader.Buffered())
+
+	str, _ := bufReader.ReadString(' ')
+	fmt.Println(str, bufReader.Buffered())
+
+	w := bufio.NewWriter(os.Stdout)
+	fmt.Fprint(w, "hello ")
+	fmt.Fprint(w, "world!")
+	w.Flush()
+}
